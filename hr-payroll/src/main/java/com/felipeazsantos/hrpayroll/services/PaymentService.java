ba1cf16ab357ba2f73efa71c807e665ffc66618c
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-03-09 08:56:38 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/felipeazsantos/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/felipeazsantos/hrpayroll/services/PaymentService.java
@@ -1,13 +1,33 @@
 package com.felipeazsantos.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.felipeazsantos.hrpayroll.entities.Payment;
+import com.felipeazsantos.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", workerId.toString());
+		
+		String url = workerHost + "/workers/{id}";
+		
+		Worker worker = restTemplate.getForObject(url, Worker.class, uriVariables);
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
